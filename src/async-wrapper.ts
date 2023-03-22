7f1b9d9a0dd4633ef764bc7eac31354473d6714c
Eddie Naff <eddienaff@gmail.com>
2023-03-22 13:59:57 -0700

    Improve types for generic async wrapper

--- src/async-wrapper.ts
+++ src/async-wrapper.ts
@@ -1,11 +1,11 @@
-export async function asyncWrapper(
-  fn: Function,
-  ...args: any
-): Promise<[unknown, any]> {
-  try {
-    const data = await fn(...args);
-    return [null, data];
-  } catch (error) {
-    return [error, null];
-  }
+export async function asyncWrapper<T>(
+    fn: (...args: any[]) => Promise<T>,
+    ...args: any
+): Promise<[unknown | null, T | null]> {
+    try {
+        const data = await fn(...args);
+        return [null, data];
+    } catch (error) {
+        return [error, null];
+    }
 }
